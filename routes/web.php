e0c580e9ef88427b3fd67595aead3ddc5d410e1c
Jordismar <jordismar@versatecnologia.com.br>
2023-11-05 14:04:11 -0300

    Exemplo de como fazer blade rota e controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MusicaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('musica')->group(function () {
+    Route::get('/', [MusicaController::class, 'index']);
 });
 
 Route::middleware([
